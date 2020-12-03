02a9f7a01d7c6079236100e3aee0b2f3d48eee47
pritam047 <pritamkar13@gmail.com>
2020-12-03 08:38:23 +0530

    Navigation added for individual reviews

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import {StyleSheet, View, Text, TouchableOpacity,FlatList} from 'react-native'
 import {globalStyles} from '../styles/global';
 
 export default function Home(){
-    return(
+    const [reviews, setReviews] = useState([
+        { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+        { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+        { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+      ]);
+
+      return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>HOME SCREEN</Text>
+          <FlatList data={reviews} renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+              <Text style={globalStyles.titleText}>{ item.title }</Text>
+            </TouchableOpacity>
+          )} />
         </View>
-    )
+      );
 }
 
